e491041740c6edb951e24a3a4b83d4b627a3b770
ByDSA <dsa21may@gmail.com>
2020-07-31 20:13:20 +0200

    outpah from File

--- src/video.js
+++ src/video.js
@@ -1,16 +1,18 @@
-const uri = require('./uri');
+const removeExtensionOf = require('./uri').removeExtensionOf;
+const getLastPartOf = require('./uri').getLastPartOf;
+const Files = require('./Files');
 const spawn = require('child_process').spawn;
 
 let ffmpeg = {};
 
 ffmpeg.exec = (params = []) => {
-    return spawn(ffmpeg.path, params);
+    return spawn(Files.paths.ffmpeg, params);
 }
 
 let m3u8 = {};
 m3u8.download = (url, opts = {}) => {
-    let outPath = opts.outPath || ffmpeg.outPath;
-    let filename = opts.filename || uri.removeExtensionOf(uri.getLastPartOf(url));
+    let outPath = opts.outPath || Files.paths.ffmpegOut;
+    let filename = opts.filename || removeExtensionOf(getLastPartOf(url));
     outPath += `/${filename}`;
     return ffmpeg.exec(["-i", url, "-c", "copy", "-bsf:a", "aac_adtstoasc", outPath]);
 };
