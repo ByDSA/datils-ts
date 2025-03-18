0104db77e145a6cf8951e26d08ea90f38c30af03
Daniel Sales <danisales.es@gmail.com>
2025-03-18 23:34:53 +0100

    auto exports generator + add patterns

--- bin/build.mjs
+++ bin/build.mjs
@@ -2,8 +2,50 @@
 
 $.verbose = true;
 
-const outDir = "build";
+const outDir = "./build";
 
 await $`rm -rf ${outDir}`;
 await $`tsc -p tsconfig-build.json`;
-await $`cp package.json pnpm-lock.yaml ./${outDir}`;
+await $`cp package.json pnpm-lock.yaml ${outDir}`;
+
+echo`Editing package.json ...`;
+await editPackageJson( {
+  scripts: null,
+  devDependencies: null,
+  exports: async () => {
+    const exports = {};
+    const files = await glob(`${outDir}/**/index.js`);
+
+    for (const file of files) {
+      const folder = file
+        .split("/")
+        .slice(2, -1)
+        .join("/");
+      const begin = folder
+        ? "./"
+        : ".";
+      let key = begin + folder;
+      let value = begin + folder + "/index.js";
+
+      exports[key] = value;
+    }
+
+    exports["./*"] = "./*.js";
+
+    return exports;
+  },
+} );
+
+async function editPackageJson(obj) {
+  const pkg = JSON.parse(await fs.readFile(`./${outDir}/package.json`, "utf8"));
+
+  for (const [key, value] of Object.entries(obj)) {
+    if (value === undefined || value === null)
+      delete pkg[key];
+
+    if (typeof value === "function")
+      pkg[key] = await value(pkg.key);
+  }
+
+  await fs.writeFile(`./${outDir}/package.json`, JSON.stringify(pkg, null, 2));
+}
