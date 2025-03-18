85a4fdb2bfcd85a157e3d7db879bd91d933e7471
Daniel Sales <danisales.es@gmail.com>
2025-03-19 00:58:09 +0100

    moved some script logic to daproj

--- bin/build.mjs
+++ bin/build.mjs
@@ -1,51 +1,42 @@
 #!/usr/bin/env zx
 
-$.verbose = true;
+import { editPackageJson } from "daproj/node/package-json/edit.mjs";
+import { defaultBuild } from "daproj/zx/build.mjs";
 
-const outDir = "./build";
-
-await $`rm -rf ${outDir}`;
-await $`tsc -p tsconfig-build.json`;
-await $`cp package.json pnpm-lock.yaml ${outDir}`;
+const { outDir } = await defaultBuild();
 
 echo`Editing package.json ...`;
-await editPackageJson( {
-  scripts: null,
-  devDependencies: null,
-  exports: async () => {
-    const exports = {};
-    const files = await glob(`${outDir}/**/index.js`);
+await editPackageJson(
+  {
+    scripts: null,
+    devDependencies: null,
+    main: "index.js",
+    files: [
+      "*",
+    ],
+    types: `${outDir}/index.d.ts`,
+    exports: async () => {
+      const exports = {};
+      const files = await glob(`${outDir}/**/index.js`);
 
-    for (const file of files) {
-      const folder = file
-        .split("/")
-        .slice(2, -1)
-        .join("/");
-      const begin = folder
-        ? "./"
-        : ".";
-      let key = begin + folder;
-      let value = begin + folder + "/index.js";
+      for (const file of files) {
+        const folder = file
+          .split("/")
+          .slice(2, -1)
+          .join("/");
+        const begin = folder
+          ? "./"
+          : ".";
+        let key = begin + folder;
+        let value = begin + folder + "/index.js";
 
-      exports[key] = value;
-    }
+        exports[key] = value;
+      }
 
-    exports["./*"] = "./*.js";
+      exports["./*"] = "./*.js";
 
-    return exports;
+      return exports;
+    },
   },
-} );
-
-async function editPackageJson(obj) {
-  const pkg = JSON.parse(await fs.readFile(`./${outDir}/package.json`, "utf8"));
-
-  for (const [key, value] of Object.entries(obj)) {
-    if (value === undefined || value === null)
-      delete pkg[key];
-
-    if (typeof value === "function")
-      pkg[key] = await value(pkg.key);
-  }
-
-  await fs.writeFile(`./${outDir}/package.json`, JSON.stringify(pkg, null, 2));
-}
+  `${outDir}/package.json`,
+);
